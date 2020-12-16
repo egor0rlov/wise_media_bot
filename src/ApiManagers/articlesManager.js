07a685eb14b959c2ad55916ca7f5cbeca40c8967
egor7orlov <egor7orlov@gmail.com>
2020-12-16 23:32:17 +0300

    Refactor

---
+++ src/ApiManagers/articlesManager.js
@@ -0,0 +1,93 @@
+require('dotenv').config();
+const fetch = require('node-fetch');
+
+exports.ArticlesManager = class {
+    constructor(bot, button) {
+        this.bot = bot;
+        this.button = button;
+        this.inlinePageNumber = 0;
+        this.nextPage = 1;
+        this.prevPage = -1;
+        this.pageNumber = 0;
+    }
+
+    async fetchArticles() {
+        const response = await fetch(process.env.ARTICLES_URL);
+        const body = await response.text();
+
+        this.articles = JSON.parse(body);
+    }
+
+    _formArticlesPage() {
+        let articlesText = ``;
+        const buttons = [[]];
+
+        const step = 10;
+        const startIndex = this.inlinePageNumber * step;
+        const endIndex = (this.articles.length - startIndex - step) < 0 ? this.articles.length : startIndex + step;
+
+        const buttonsInRowAmount = 5;
+        let indexOfCurrentRow = 0;
+
+        for (let i = startIndex; i < endIndex; i++) {
+            const article = this.articles[i];
+            const articleNumeration = ((i % step) + 1); //From 1 to 10.
+
+            articlesText = articlesText.concat(`<b>${articleNumeration}:</b> ${article.name}`) + '\n';
+
+            buttons[indexOfCurrentRow].push({text: articleNumeration, callback_data: article.queue});
+
+            if (buttons[indexOfCurrentRow].length === buttonsInRowAmount) {
+                buttons.push([]);
+                indexOfCurrentRow++;
+            }
+        }
+
+        const arrowButtons = this._formNavigationButtons();
+
+        buttons.push(arrowButtons);
+
+        return {text: articlesText, keyboard: buttons};
+    }
+
+    _formNavigationButtons() {
+        const currentPage = this.inlinePageNumber + 1;
+        const arrowButtons = [{text: '📄: ' + currentPage, callback_data: this.pageNumber}];
+        const lastArticlesPage = Math.ceil(this.articles.length / 10);
+
+        if (currentPage !== 1) {
+            arrowButtons.unshift({text: this.button.arrowPrevious, callback_data: this.prevPage});
+        }
+
+        if (currentPage !== lastArticlesPage) {
+            arrowButtons.push({text: this.button.arrowNext, callback_data: this.nextPage});
+        }
+
+        return arrowButtons;
+    }
+
+    async sendArticlesList(msg) {
+        const chatId = msg.chat.id;
+        const data = this._formArticlesPage();
+
+        await this.bot.sendMessage(chatId, data.text, {
+            parse_mode: 'HTML',
+            reply_markup: {
+                inline_keyboard: data.keyboard
+            }
+        });
+    }
+
+    async updateInlineMessage(query) {
+        const data = query.data;
+        this.inlinePageNumber = this.inlinePageNumber + Number(data);
+
+        const chatId = query.message.chat.id;
+        const messageId = query.message.message_id;
+        const articlesData = this._formArticlesPage();
+
+        await this.bot.editMessageText(articlesData.text, {message_id: messageId, chat_id: chatId, parse_mode: 'HTML'});
+        await this.bot.editMessageReplyMarkup({inline_keyboard: articlesData.keyboard},
+            {message_id: messageId, chat_id: chatId});
+    }
+}
---
+++ src/ApiManagers/newsManager.js
@@ -0,0 +1,69 @@
+require('dotenv').config();
+
+const NewsAPI = require('newsapi');
+const NEWS_TOKEN = process.env.NEWS_API_TOKEN;
+const news = new NewsAPI(NEWS_TOKEN);
+const dayjs = require('dayjs');
+const {BotAnswer} = require('../strings');
+
+exports.NewsManager = class {
+    constructor(bot) {
+        this.bot = bot;
+        this.pastWeek = dayjs().subtract(1, 'week').format('YYYY-MM-DD');
+        this.requestArticlesAmount = 5;
+    }
+
+    async fetchNewsFromWeb(requestText) {
+        const articles = await news.v2.everything({
+            q: requestText,
+            sortBy: 'relevancy',
+            from: this.pastWeek
+        }).then(response => response.articles);
+
+        this.news = articles;
+    }
+
+    async sendNews(chatId) {
+        const newsAmount = this.news.length < this.requestArticlesAmount ? this.news.length : this.requestArticlesAmount;
+
+        for (let i = 0; i <= newsAmount - 1; i++) {
+            const article = this.news[i];
+
+            const source = article.source.name;
+            const title = article.title;
+            const lead = article.description;
+            const date = dayjs(article.publishedAt).format('DD/MM/YYYY');
+            const url = article.url;
+
+            const html = this._getArticleInfoHtml(i, source, title, lead, date, url);
+
+            try {
+                await this.bot.sendMessage(chatId, html, {
+                    parse_mode: 'HTML'
+                });
+            } catch (e) {
+                await this.bot.sendMessage(chatId, BotAnswer.invalidDataNews);
+
+                console.error(e);
+            }
+        }
+    }
+
+    _getArticleInfoHtml(articleIndex, source, title, lead, date, url) {
+        const html = `
+<b>${articleIndex + 1}:</b>
+
+<i>Ресурс: ${source}</i>
+
+<b>${title}</b>
+
+${lead}
+
+<i>${date}</i>
+
+<b><a href="${url}">Посилання</a></b>
+        `;
+
+        return html;
+    }
+}
